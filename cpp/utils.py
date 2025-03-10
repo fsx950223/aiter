99fcc194786713117eda9207dcba07d7668f923a
fsx950223 <fsx950223@outlook.com>
2025-03-10 03:37:43 +0000

    forward compitable

--- cpp/utils.py
+++ cpp/utils.py
@@ -4,7 +4,8 @@ import subprocess
 from jinja2 import Template
 import ctypes
 import importlib
-
+from packaging.version import parse, Version
+import psutil
 # AITER_CACHE_DIR=os.environ.get("AITER_CACHE_DIR", "./")
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -34,6 +35,38 @@ BUILD_DIR=os.path.abspath(os.path.join(AITER_ROOT_DIR, "build"))
 
 libs = {}
 
+
+def get_max_jobs():
+    max_num_jobs_cores = int(max(1, os.cpu_count()*0.8))
+    # calculate the maximum allowed NUM_JOBS based on free memory
+    free_memory_gb = psutil.virtual_memory().available / \
+        (1024 ** 3)  # free memory in GB
+    # each JOB peak memory cost is ~8-9GB when threads = 4
+    max_num_jobs_memory = int(free_memory_gb / 9)
+
+    # pick lower value of jobs based on cores vs memory metric to minimize oom and swap usage during compilation
+    max_jobs = max(1, min(max_num_jobs_cores, max_num_jobs_memory))
+    return max_jobs
+
+
+def get_hip_version():
+    version = subprocess.run(f"/opt/rocm/bin/hipconfig --version", shell=True, capture_output=True, text=True)
+    return parse(version.stdout.split()[-1].rstrip('-').replace('-', '+'))
+
+
+def validate_and_update_archs():
+    archs = os.getenv("GPU_ARCHS", "native").split(";")
+    # List of allowed architectures
+    allowed_archs = ["native", "gfx90a",
+                     "gfx940", "gfx941", "gfx942", "gfx1100"]
+
+    # Validate if each element in archs is in allowed_archs
+    assert all(
+        arch in allowed_archs for arch in archs
+    ), f"One of GPU archs of {archs} is invalid or not supported"
+    return archs
+
+
 def init_build_dir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
@@ -61,10 +94,46 @@ def compile_lib(src_file, folder, includes=[], sources=[], cxxflags=["-O3", "-st
     with open(f"{BUILD_DIR}/{folder}/call_lib.cpp", "w") as f:
         f.write(src_file)
     sources += ["call_lib.cpp"]
+    cxxflags += [
+            "-O3", "-std=c++17",
+            "-DLEGACY_HIPBLAS_DIRECT",
+            "-DUSE_PROF_API=1",
+            "-D__HIP_PLATFORM_HCC__=1",
+            "-D__HIP_PLATFORM_AMD__=1",
+            "-U__HIP_NO_HALF_CONVERSIONS__",
+            "-U__HIP_NO_HALF_OPERATORS__",
+
+            "-mllvm", "-enable-post-misched=0",
+            "-mllvm", "-amdgpu-early-inline-all=true",
+            "-mllvm", "-amdgpu-function-calls=false",
+            "-mllvm", "--amdgpu-kernarg-preload-count=16",
+            "-mllvm", "-amdgpu-coerce-illegal-types=1",
+            # "-v", "--save-temps",
+            "-Wno-unused-result",
+            "-Wno-switch-bool",
+            "-Wno-vla-cxx-extension",
+            "-Wno-undefined-func-template",
+
+            "-fgpu-flush-denormals-to-zero",
+    ]
+
+    # Imitate https://github.com/ROCm/composable_kernel/blob/c8b6b64240e840a7decf76dfaa13c37da5294c4a/CMakeLists.txt#L190-L214
+    hip_version = get_hip_version()
+    if hip_version > Version('5.7.23302'):
+        cxxflags += ["-fno-offload-uniform-block"]
+    if hip_version > Version('6.1.40090'):
+        cxxflags += ["-mllvm", "-enable-post-misched=0"]
+    if hip_version > Version('6.2.41132'):
+        cxxflags += ["-mllvm", "-amdgpu-early-inline-all=true",
+                        "-mllvm", "-amdgpu-function-calls=false"]
+    if hip_version > Version('6.2.41133') and hip_version < Version('6.3.00000'):
+        cxxflags += ["-mllvm", "-amdgpu-coerce-illegal-types=1"]
+    archs = validate_and_update_archs()
+    cxxflags+=[f"--offload-arch={arch}" for arch in archs]
     makefile_file = makefile_template.render(includes=includes, sources=sources, cxxflags=cxxflags)
     with open(f"{BUILD_DIR}/{folder}/Makefile", "w") as f:
         f.write(makefile_file)
-    subprocess.run(f"cd {BUILD_DIR}/{folder} && make build -j8", shell=True)
+    subprocess.run(f"cd {BUILD_DIR}/{folder} && make build -j{get_max_jobs()}", shell=True)
 
 def run_lib(folder, *args):
     if folder in libs:
